b54584d775470a9bc0d52496190bddb6949f6e84
Esteban García <151795541+MixDark@users.noreply.github.com>
2025-02-05 11:27:52 -0500

    Update GeneradorContraseñas.py

--- Generador contraseñas/GeneradorContraseñas.py
+++ Generador contraseñas/GeneradorContraseñas.py
@@ -1,15 +1,30 @@
-import string
 import random
+import string
 import pyperclip
 
 class PasswordManager:
     def __init__(self):
-        self.password = ""
+        self.last_password = ""
 
-    def generate_password(self, length):
-        caracteres = string.ascii_letters + string.digits + string.punctuation
-        self.password = ''.join(random.choice(caracteres) for _ in range(length))
-        return self.password
+    def generate_password(self, length, options):
+        # Crear el conjunto de caracteres basado en las opciones seleccionadas
+        chars = ''
+        if options['mayusculas']:
+            chars += string.ascii_uppercase
+        if options['minusculas']:
+            chars += string.ascii_lowercase
+        if options['numeros']:
+            chars += string.digits
+        if options['especiales']:
+            chars += "!@#$%^&*"
+
+        if not chars:
+            raise ValueError("Debe seleccionar al menos un tipo de carácter")
+
+        # Generar la contraseña
+        self.last_password = ''.join(random.choice(chars) for _ in range(length))
+        return self.last_password
 
     def copy_password(self):
-        pyperclip.copy(self.password)
+        if self.last_password:
+            pyperclip.copy(self.last_password)
